990f0e013b9e90bc389ee9e6eaa32d83dd580ca2
Connor Bailey <bailey.connor.p@gmail.com>
2019-01-17 18:45:28 -0600

    comments added to describe work. Shopping cart remove all items bug fixed, allowing items to be re entered into the cart

--- scripts/shoppingCart.js
+++ scripts/shoppingCart.js
@@ -7,12 +7,14 @@ const displayShoppingCart = () => {
     let grandTotal = 0
 
     shoppingCart.forEach((product, idx) => {
-
+        // creates the html for dropdown option tags from 1 to the amount of the product in the cart
         let removeQtyDropdownHTML = "";
         for (let i = 1; i <= product.counter; i++) {
             removeQtyDropdownHTML += `<option value=${i}>${i}</option>`
         }
-
+        
+        // product.counter added to keep track of how many in of each item in the cart. price changed to total price to reflect the number of the product in the cart. 
+        // removeQtyDropdownHTML inserted so that each dropdown only goes as high as the quantity of each product in the cart
         cartEl.innerHTML +=
             `
         <section class="shoppingCart__item">
@@ -33,7 +35,7 @@ const displayShoppingCart = () => {
 
         grandTotal += product.totalPrice;
     })
-
+    // empty shopping cart button added
     cartEl.innerHTML += `
       <h3 class="orderTotalPrice">You owe us: ${grandTotal.toLocaleString("en-US", {
         style: "currency",
@@ -42,7 +44,7 @@ const displayShoppingCart = () => {
     <button id="emptyShoppingCart">Remove All Items From Cart</button>
     `
 
-    // Get a reference to all purchase buttons
+    // Get a reference to all buttons
     const allRemoveButtons = document.querySelectorAll(".cart_removeButton")
     const allremoveQtyButtons = document.querySelectorAll(".cart_removeQtyButton")
 
@@ -52,22 +54,27 @@ const displayShoppingCart = () => {
             "click",
             (event) => {
                 const indexToRemove = parseInt(event.target.id)
+                shoppingCart[indexToRemove].counter = 0;
                 shoppingCart.splice(indexToRemove, 1)
                 displayShoppingCart()
             }
         )
     }
+    // this creates a button to remove the quantity (from the dropdown) of an item from the cart
     for (const button of allremoveQtyButtons) {
         button.addEventListener(
             "click",
             (event) => {
+                // strips excess text to get down to the products index in the shopping cart
                 let shortenedIdIndex = event.target.id.replace("removeQty", "")
                 let removeDropdownQtyValue = parseInt(document.querySelector(`#dropdown${shortenedIdIndex}`).value);
+                // removes a quantity less than the total quantity in the cart
                 if (shoppingCart[shortenedIdIndex].counter > removeDropdownQtyValue) {
                     shoppingCart[shortenedIdIndex].counter -= removeDropdownQtyValue;
                     shoppingCart[shortenedIdIndex].totalPrice -= (shoppingCart[shortenedIdIndex].price * removeDropdownQtyValue);
                     displayShoppingCart()
                 } else {
+                    // removes the item from the shopping cart if the entire qty is removed
                     shoppingCart[shortenedIdIndex].counter = 0;
                     shoppingCart.splice(shortenedIdIndex,1);
                     displayShoppingCart();
@@ -75,10 +82,14 @@ const displayShoppingCart = () => {
             }
         )
     }
-
+    // emptys the whole shopping cart if the button is clicked.
     document.querySelector("#emptyShoppingCart").addEventListener(
         "click",
         (event) => {
+            // allows product to be re-entered into the cart after being removed
+            shoppingCart.forEach(product => {
+                product.counter = 0;
+            });
             shoppingCart.splice(0,shoppingCart.length);
             displayShoppingCart();
         }
